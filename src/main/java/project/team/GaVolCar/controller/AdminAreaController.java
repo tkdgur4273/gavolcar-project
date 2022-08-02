552f2e22877e54586312b42a43f5d5480a2fae5c
DESKTOP-D4P20FS\skype <sprkahxhalcl@naver.com>
2022-08-02 10:15:40 +0900

    Revert "no message"
    This reverts commit 1aab1a47c9b0984505896abf88e8b1a4c9d471f0.

--- src/main/java/project/team/GaVolCar/controller/AdminAreaController.java
+++ src/main/java/project/team/GaVolCar/controller/AdminAreaController.java
@@ -42,7 +42,7 @@ public class AdminAreaController {
 		log.info("rest_area_view()...");
 		
 		String area_name = areaVO.getArea_name();
-		model.addAttribute("name", areaService.areaRead(area_name));
+		model.addAttribute("name", areaService.getAreaInfo(area_name));
 		
 		return "area/admin/rest_area_content_view";
 	}
